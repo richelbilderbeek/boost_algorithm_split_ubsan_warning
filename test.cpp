af29919cb62f266c16af3a35b43ed77c14e54a17
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-03 13:46:03 +0200

    Remove pragma's

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 // Boost.Test does not play well with -Weffc++
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weffc++"
+
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(test)
